4c181273adef658653048fc74bafa7882d283016
Tanichael <toma92toop@gmail.com>
2023-11-24 23:08:00 +0900

    add unsafe test

--- ConsoleCsharp/Program.cs
+++ ConsoleCsharp/Program.cs
@@ -6,13 +6,61 @@ using System.Threading.Tasks;
 using static System.Console;
 using System.Runtime.CompilerServices;
 
-class X
+namespace TestApp
 {
-    public int Value;
+    class X
+    {
+        public int Value;
+    }
+
+    unsafe class Program
+    {
+        static ulong AsUnmanaged<T>(T r) where T : class => (ulong)Unsafe.As<T, IntPtr>(ref r);
+
+        static ulong AsUnmanaged<T>(ref T r) => (ulong)Unsafe.AsPointer(ref r);
+
+        static void Main()
+        {
+            void GenerateGarbage()
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    var dummy = new object();
+                }
+            }
+
+            GenerateGarbage();
+
+            var x = new X { Value = 12345678 };
+            ref var r = ref x.Value;
+
+            var addressOfX = AsUnmanaged(x);
+            var addressOfValue = AsUnmanaged(ref r);
+
+            WriteLine((addressOfX, addressOfValue));
+
+            GenerateGarbage();
+
+            GC.Collect(0, GCCollectionMode.Forced);
+            WriteLine("---GC発生---");
+
+            WriteLine("unmanaged " + (addressOfX, addressOfValue));
+
+            WriteLine("managed " + (AsUnmanaged(x), AsUnmanaged(ref r)));
+
+            fixed (int* p = &x.Value)
+            {
+                GenerateGarbage();
+                GC.Collect(0, GCCollectionMode.Forced);
+                WriteLine("---GC発生---");
+                WriteLine("managed " + (AsUnmanaged(x), AsUnmanaged(ref r)));
+            }
+
+            UnsafeTest.Execute();
+
+            ReadKey();
+        }
+
+    }
 }
 
-unsafe class Program
-{
-    
-
-}
---
+++ ConsoleCsharp/UnsafeTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TestApp
+{
+    struct Point
+    {
+        public short X;
+        public short Y;
+    }
+
+    public class UnsafeTest
+    {
+        public static void Execute()
+        {
+            Console.WriteLine("execute");
+
+            /*var p = new Point();
+
+            var pp = &p;
+
+            int* pi = (int*)pp;
+            *pi = 0x00010002;
+
+            Console.WriteLine(pp->X);
+            Console.WriteLine(pp->Y);
+
+            byte* pb = (byte*)pp;
+
+            Console.WriteLine(pb[0]);
+            Console.WriteLine(pb[1]);
+            Console.WriteLine(pb[2]);
+            Console.WriteLine(pb[3]);*/
+        }
+    }
+}
+

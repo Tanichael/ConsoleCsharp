9ff9e3025452ab6545c3fc3bfe8aacc38e0b20c7
Tanichael <toma92toop@gmail.com>
2023-11-24 21:31:37 +0900

    first commit

---
+++ ConsoleCsharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Runtime.CompilerServices;
+
+class X
+{
+    public int Value;
+}
+
+unsafe class Program
+{
+    
+
+}
